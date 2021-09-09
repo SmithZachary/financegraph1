232ac3e1c43fce5fc09938fd3e80d694ed93a341
SmithZachary <zachary.6.smith@gmail.com>
2021-09-09 15:54:54 -0500

    Initial Commit

---
+++ finance/financeApp.swift
@@ -0,0 +1,17 @@
+//
+//  financeApp.swift
+//  finance
+//
+//  Created by Zach Smith on 9/9/21.
+//
+
+import SwiftUI
+
+@main
+struct financeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
